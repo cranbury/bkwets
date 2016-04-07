dc759c9e6015ded508ff8c00c095a36c9fcca364
Granger <GrangerAbuhoff@gmail.com>
2016-04-07 11:18:18 -0400

    add del

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,6 +17,17 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
   end
 
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+
+    respond_to do |format|
+      format.html { redirect_to games_url }
+      format.json { head :no_content }
+      format.js   { render :layout => false }
+    end
+  end
+
   private
   def game_params
     params.require(:game).permit(:name, :address)
